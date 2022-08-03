ac89d5d3128a0cd3b0224bd9ecc606a18dd20376
Majey <kevinerick78@yahoo.com>
2022-08-03 20:47:27 +0300

    Implementing Reads: The “R” in CRUD

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 from flask import Flask, render_template
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 # from migrate import Migrate
 
 app = Flask(__name__)
-# db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:ICUI4CU1997@localhost:5432/todoapp"
+db = SQLAlchemy(app)
 # migrate = Migrate(app, db)
+
+class Todo(db.Model):
+    __tablename__ = "todos"
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<Todo {self.id}, {self.description}>"
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data = [
-        {
-        "description": "to do 1"
-        },
-        {
-        "description": "to do 2"
-        },
-        {
-        "description": "to do 3"
-        },
-        {
-        "description": "to do 4"
-        },
-    ])
+    return render_template('index.html', data = Todo.query.all())
 
 
